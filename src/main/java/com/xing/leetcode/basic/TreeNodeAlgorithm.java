54646c49a3479a886d571c86127d5e12dc29531f
zhangx <xing.zhang@ezhiyang.com>
2021-05-25 10:41:31 +0800

    init

--- src/main/java/com/xing/leetcode/basic/TreeNodeAlgorithm.java
+++ src/main/java/com/xing/leetcode/basic/TreeNodeAlgorithm.java
@@ -8,13 +8,14 @@ import com.xing.leetcode.basic.node.TreeNode;
 public class TreeNodeAlgorithm {
     public static void main(String[] args) {
         TreeNode treeNode = new TreeNode(3);
-        treeNode.left = new TreeNode(9);
-        treeNode.right = new TreeNode(20);
+        treeNode.left = new TreeNode(1);
+        treeNode.right = new TreeNode(7);
         treeNode.left.left = null;
         treeNode.right.right = null;
-        treeNode.right.left = new TreeNode(15);
-        treeNode.right.right = new TreeNode(7);
-        System.out.println(maxDepth(treeNode));
+        treeNode.right.left = new TreeNode(6);
+        treeNode.right.right = new TreeNode(15);
+//        System.out.println(maxDepth(treeNode));
+        System.out.println(isValidBST(treeNode));
     }
 
     /**
@@ -32,5 +33,40 @@ public class TreeNodeAlgorithm {
             return 0;
         }
         return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+
+    /**
+     * 给定一个二叉树，判断其是否是一个有效的二叉搜索树。
+     *
+     * 假设一个二叉搜索树具有如下特征：
+     *
+     * 节点的左子树只包含小于当前节点的数。
+     * 节点的右子树只包含大于当前节点的数。
+     * 所有左子树和右子树自身必须也是二叉搜索树。
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xn08xg/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @param root
+     * @return
+     */
+    private static boolean isValidBST(TreeNode root) {
+        //区间递归
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    private static boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) {
+            return true;
+        }
+        //每个节点如果超过这个范围，直接返回false
+        if (root.val >= maxVal || root.val <= minVal) {
+            return false;
+        }
+        //这里再分别以左右两个子节点分别判断，
+        //左子树范围的最小值是minVal，最大值是当前节点的值，也就是root的值，因为左子树的值要比当前节点小
+        //右子数范围的最大值是maxVal，最小值是当前节点的值，也就是root的值，因为右子树的值要比当前节点大
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
 }
