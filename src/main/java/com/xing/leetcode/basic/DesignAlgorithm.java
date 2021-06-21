6a4126c778fea1cd83f35ef59727ff781da4a139
zhangx <xing.zhang@ezhiyang.com>
2021-06-21 13:26:36 +0800

    3的幂

--- src/main/java/com/xing/leetcode/basic/DesignAlgorithm.java
+++ src/main/java/com/xing/leetcode/basic/DesignAlgorithm.java
@@ -1,8 +1,5 @@
 package com.xing.leetcode.basic;
 
-import com.xing.leetcode.basic.node.ListNode;
-
-import java.util.Arrays;
 import java.util.Random;
 
 /**
@@ -12,7 +9,8 @@ public class DesignAlgorithm {
     public static void main(String[] args) {
         int[] arr = {1, 5, 8, 2, 7, 6, 3};
 //        System.out.println(Arrays.toString(shuffle(arr)));
-        System.out.println(countPrimes(5));
+//        System.out.println(countPrimes(5));
+        System.out.println();
     }
 
     /**
@@ -64,5 +62,25 @@ public class DesignAlgorithm {
             }
         }
         return count;
+    }
+
+    /**
+     * 给定一个整数，写一个函数来判断它是否是 3的幂次方。如果是，返回 true ；否则，返回 false 。
+     *
+     * 整数 n 是 3 的幂次方需满足：存在整数 x 使得 n == 3x
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnsdi2/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * 题中n的范围是-2^31 <= n <= 2^31 - 1，而在这个范围内3的最大幂是1162261467，
+     * 在比他大就超过int表示的范围了，我们直接用它对n求余即可，过求余的结果是0，说明n是3的幂次方
+     *
+     * @param n
+     * @return
+     */
+    private static boolean isPowerOfThree(int n) {
+        return n > 0 && 1162261467 % n == 0;
     }
 }
