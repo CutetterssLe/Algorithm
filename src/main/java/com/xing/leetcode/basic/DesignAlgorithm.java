63bed4debd08bdc543e3e50ec3bc130968aa64e8
zhangx <xing.zhang@ezhiyang.com>
2021-06-29 09:25:00 +0800

    有效括号

--- src/main/java/com/xing/leetcode/basic/DesignAlgorithm.java
+++ src/main/java/com/xing/leetcode/basic/DesignAlgorithm.java
@@ -13,7 +13,8 @@ public class DesignAlgorithm {
 //        System.out.println(isPowerOfThree(2));
 //        System.out.println(romeNumConvertInt("MCMXCIV"));
 //        System.out.println(hammingDistance(1,4));
-        System.out.println(reverseBits(01));
+//        System.out.println(reverseBits(01));
+        System.out.println(isValid("(({{}}))"));
     }
 
     /**
@@ -234,5 +235,29 @@ public class DesignAlgorithm {
         }
 
         return res;
+    }
+
+    /**
+     *给定一个只包括 '('，')'，'{'，'}'，'['，']'的字符串 s ，判断字符串是否有效。
+     *
+     * 有效字符串需满足：
+     *
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnbcaj/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     * @return
+     */
+    private static boolean isValid(String s) {
+        int count = s.length() / 2 ;
+        for (int i = 0; i < count; i++) {
+            s = s.replaceAll("\\(\\)", "");
+            s = s.replaceAll("\\[]", "");
+            s = s.replaceAll("\\{}", "");
+        }
+        return s.equals("");
     }
 }
