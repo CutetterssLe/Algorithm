9ce78e7c08e3f4029197712e3e6fbd5c0ffa15d7
zhangx <xing.zhang@ezhiyang.com>
2021-07-01 14:30:29 +0800

    丢失的数字

--- src/main/java/com/xing/leetcode/basic/DesignAlgorithm.java
+++ src/main/java/com/xing/leetcode/basic/DesignAlgorithm.java
@@ -9,7 +9,7 @@ import java.util.Random;
  */
 public class DesignAlgorithm {
     public static void main(String[] args) {
-        int[] arr = {1, 5, 8, 2, 7, 6, 3};
+        int[] arr = {1, 0, 2, 4, 5, 7, 3};
 //        System.out.println(Arrays.toString(shuffle(arr)));
 //        System.out.println(countPrimes(5));
 //        System.out.println(isPowerOfThree(2));
@@ -17,7 +17,8 @@ public class DesignAlgorithm {
 //        System.out.println(hammingDistance(1,4));
 //        System.out.println(reverseBits(01));
 //        System.out.println(isValid("(({{}}))"));
-        System.out.println(generate(5));
+//        System.out.println(generate(5));
+        System.out.println(missingNumber(arr));
     }
 
     /**
@@ -286,5 +287,36 @@ public class DesignAlgorithm {
             res.add(new ArrayList<>(row));
         }
         return res;
+    }
+
+    /**
+     * 给定一个包含 [0, n] 中 n 个数的数组 nums ，找出 [0, n] 这个范围内没有出现在数组中的那个数。
+     * 输入：nums = [3,0,1]
+     * 输出：2
+     * 解释：n = 3，因为有 3 个数字，所以所有的数字都在范围 [0,3] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+     *
+     * 输入：nums = [0,1]
+     * 输出：2
+     * 解释：n = 2，因为有 2 个数字，所以所有的数字都在范围 [0,2] 内。2 是丢失的数字，因为它没有出现在 nums 中。
+     *
+     * 作者：力扣 (LeetCode)
+     * 链接：https://leetcode-cn.com/leetbook/read/top-interview-questions-easy/xnj4mt/
+     * 来源：力扣（LeetCode）
+     * 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+     *
+     * a ^ a = 0; 0 ^ b = b; 所以 a ^ b ^ a = a ^ a ^ b = b
+     *
+     * 等差数列，0 ~ n 的和 减去 当前数组内所有数字的和，就是缺失的数字
+     * @return
+     */
+    private static int missingNumber(int[] nums) {
+        if (nums.length <= 0) {
+            return 0;
+        }
+        int reduce = 0;
+        for (int i = 0; i < nums.length; i++) {
+            reduce = reduce ^ nums[i] ^ i;
+        }
+        return reduce ^ nums.length;
     }
 }
